c677d89e5b143554822387ed2ea8df621fbe77e8
Milad Rezazadeh <78522933+miladrezazadeh@users.noreply.github.com>
2021-11-23 07:36:41 -0500

    Add files via upload

---
+++ app_utilities.py
@@ -0,0 +1,42 @@
+## Import requried libraries
+import clean_utilities as CU
+## for data
+import numpy as np
+import pandas as pd
+
+## Neglect the warnings!
+import warnings
+warnings.filterwarnings("ignore")
+
+## for saving and loading model
+import pickle
+
+## for word embedding with Spacy
+import spacy
+import en_core_web_lg
+
+
+def tweet_prediction(tweet):
+    test_tweet = tweet
+    clean_tweet = []
+    clean_tweet.append(CU.tweets_cleaner(test_tweet))
+    ## load English model of Spacy
+    nlp = en_core_web_lg.load()
+    ## word-embedding
+    test = pd.np.array([pd.np.array([token.vector for token in nlp(s)]).mean(axis=0) * pd.np.ones((300)) \
+                        for s in clean_tweet])
+    ## Load the model
+    SVM = \
+        "../models/model_svm1.pkl"
+    with open(SVM, 'rb') as file3:
+        clf = pickle.load(file3)
+
+    ## prediction
+    labels_pred = clf.predict(test)
+
+    # if labels_pred[0] == 1:
+    #     pred = "Depressive"
+    # else:
+    #     pred = "Non-depressive"
+
+    return labels_pred[0]
